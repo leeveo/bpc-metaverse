311cf57007b7a8e86a205b3f339df41c833ff07c
Leeveo <101552154+leeveo@users.noreply.github.com>
2025-03-06 10:39:23 +0100

    commit

--- src/app/resources/content.ts
+++ src/app/resources/content.ts
@@ -45,14 +45,6 @@ const home = {
   ),
 };
 
-const blog = {
-  label: "Blog",
-  title: "Writing about design and tech...",
-  description: `Read what ${person.name} has been up to recently`,
-  // Create new blog posts by adding a new .mdx file to app/blog/posts
-  // All posts will be listed on the /blog route
-};
-
 const work = {
   label: "Toutes les salles",
   title: "Toutes les salles du Metaverse",
@@ -149,4 +141,4 @@ const metaverseGallery = {
   ],
 };
 
-export { person, social, newsletter, home, blog, work, gallery, metaverseGallery };
+export { person, social, newsletter, home, work, gallery, metaverseGallery };
